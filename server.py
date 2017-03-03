84f20f5843dd669c77063ad9cd12b033e38fe7f5
amakarov <amakarov@npoprogress.com>
2017-03-03 18:12:14 +0300

    init commit

---
+++ server.py
@@ -0,0 +1,77 @@
+
+
+import asyncio
+import websockets
+from threading import Thread
+import requests
+import time
+from lxml import etree
+
+R_LIST = {
+
+    1:"EURUSD",
+    2:"USDJPY"
+}
+
+
+rates = []
+
+
+
+def parseXML(data):
+
+    xml = etree.fromstring(data.encode("utf-8"))
+    nodes = xml.xpath('/Rates/Rate')
+
+    d = {}
+    for node in nodes:
+        i = [x.text for x in node.getchildren()]
+        d[i[0]] = (float(i[1]) + float(i[2])) / 2.0
+
+    return d
+
+def parseRE(data):
+    pass
+
+
+def get_rates(rates):
+
+    while True:
+        res = requests.get("http://rates.fxcm.com/RatesXML2", timeout=0.5)
+        time.sleep(1)
+        rates.append(parseXML(res.text))
+
+        #write to redis
+
+
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("< {}".format(name))
+
+    greeting = "Hello {}!".format(name)
+    await websocket.send(greeting)
+
+    while True:
+        import pdb; pdb.set_trace()  # XXX BREAKPOINT
+
+
+        await websocket.send(rates[:-1])
+        await asyncio.sleep(1.0)
+
+
+def main():
+
+    t = Thread(target=get_rates, args=(rates,))
+    t.start()
+
+
+    start_server = websockets.serve(hello, 'localhost', 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+    main()
