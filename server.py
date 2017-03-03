3cdaf2ba221854efdd52e1b2e7332cd8cb59260c
anatolii <anatolii.makaroff@gmail.com>
2017-03-04 02:05:45 +0300

    add new features

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
-
+#
+# Test ex. Websocket Rate Server
+#
+# Authtor Makarov A
+# Date    04/03/17
+#
 
 import asyncio
 import websockets
@@ -6,23 +11,40 @@ from threading import Thread
 import requests
 import time
 from lxml import etree
+import logging
+
+
+#
+# config
+#
 
 R_LIST = {
-
-    1:"EURUSD",
-    2:"USDJPY"
+    1: "EURUSD",
+    2: "USDJPY"
 }
+
+HOST = "localhost"
+PORT = 8080
+
+FX_URL = "http://rates.fxcm.com/RatesXML2"
+
+######
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 rates = []
 
 
-
 def _time_log(f):
+    """
+    time measure decorator
+    """
     def wrap(*args, **kwargs):
         tb = time.time()
         ret = f(*args, **kwargs)
-        print("elapsed: {}".format(time.time() - tb))
+        logger.debug("time elapsed: {} {} s.".format(f.__name__, time.time() - tb))
         return ret
     return wrap
 
@@ -35,26 +57,76 @@ def parseXML(data):
     d = {}
     for node in nodes:
         i = [x.text for x in node.getchildren()]
-        d[i[0]] = (float(i[1]) + float(i[2])) / 2.0
+
+        if i[0] in R_LIST.values():
+            d[i[0]] = (float(i[1]) + float(i[2])) / 2.0
+        else:
+            continue
 
     return d
 
 def parseRE(data):
+    """
+    TODO: maybe regexp be more faster parsing way
+    """
     pass
 
 
+
+def b_time():
+    """ begin date memorizer with skip"""
+    tb = time.time()
+    while True:
+        i = (yield)
+
+        if i:
+            tb = time.time()
+
+        yield tb
+
+
+@_time_log
 def get_rates(rates):
 
+    s = requests.Session()
+
+    # tb = b_time()
+    # tb.send(None)
+
     while True:
-        res = requests.get("http://rates.fxcm.com/RatesXML2", timeout=0.05)
-        time.sleep(1)
-        rates.append(parseXML(res.text))
+        tb = time.time()
+        try:
+            res = s.get(FX_URL, timeout=0.05)
+            if res.status_code != 200:
+                raise Exception(res.text)
 
-        #write to redis
+            # perse result and put to cache
+            rates.append(parseXML(res.text))
+
+        except Exception as e:
+            logger.error(e)
+            # try immedeatly 50 ms
+            time.sleep(0.050)
+            logger.debug("try to reconnect..")
+            continue
+
+        _sleep1(time.time() - tb)
+
+
+def _sleep1(delta):
+    #
+    # get more fair interval
+    #
+    logger.debug("finish ops for: {}".format(delta))
+    if delta >= 1.0:
+        logger.warn("time is out..")
+
+    else:
+        time.sleep(1.0 - delta)
 
 
 
-async def hello(websocket, path):
+async def h_rate(websocket, path):
     name = await websocket.recv()
     print("< {}".format(name))
 
@@ -71,14 +143,16 @@ async def hello(websocket, path):
 
 def main():
 
+    #
+    # start lister for rates
+    # it pull, write & save to redis rates
+    #
     t = Thread(target=get_rates, args=(rates,))
     t.start()
 
 
-    start_server = websockets.serve(hello, 'localhost', 8765)
-
+    start_server = websockets.serve(h_rate, HOST, PORT)
     asyncio.get_event_loop().run_until_complete(start_server)
-
     asyncio.get_event_loop().run_forever()
 
 
